1e3481745a517ece96b892a6b714f50c3c9f7201
forellko <dragonforce97q@gmail.com>
2023-11-03 22:24:30 +0300

    Add mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 import Navbar from './(components)/Navbar';
 
+interface MockDataItem {
+  title: string;
+  description: string;
+}
+
 export default function Home() {
+  const mockData: MockDataItem[] = [
+    { title: 'title1', description: 'description1' },
+    { title: 'title2', description: 'description2' },
+    { title: 'title3', description: 'description3' },
+    { title: 'title4', description: 'description4' },
+    { title: 'title5', description: 'description5' },
+  ];
+
   return (
     <div className="flex flex-col">
       <Navbar />
+      
     </div>
   );
 }
